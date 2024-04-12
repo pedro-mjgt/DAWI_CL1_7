16aab99da91dc175b8633c347210c4c83977e4ad
BrianAnton <151844246+BrianAnton@users.noreply.github.com>
2024-04-12 15:36:21 -0500

    Update CalculadoraController.java

--- src/main/java/pe/edu/cibertec/DAWI_CL1_7/controller/CalculadoraController.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_7/controller/CalculadoraController.java
@@ -1,14 +1,14 @@
 package pe.edu.cibertec.DAWI_CL1_7.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pe.edu.cibertec.DAWI_CL1_XXX.model.ResultadoCalculo;
+import pe.edu.cibertec.DAWI_CL1_7.model.ResultadoCalculo;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.ui.Model;
 @Controller
 public class CalculadoraController {
     @GetMapping("/calculadora")
