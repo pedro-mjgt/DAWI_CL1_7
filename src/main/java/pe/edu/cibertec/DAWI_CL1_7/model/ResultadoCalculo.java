14b2c2a2715d8000bbb3e8854e80c3914b5e256a
BrianAnton <151844246+BrianAnton@users.noreply.github.com>
2024-04-12 15:38:11 -0500

    Create ResultadoCalculo.java

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_7/model/ResultadoCalculo.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.DAWI_CL1_7.model;
+
+import lombok.Data;
+
+@Data
+public class ResultadoCalculo {
+    private int numero;
+    private int cuadrado;
+    private double mitad;
+}
